6c36987f8fcea7b5964a4b6923057166134c8834
Asad-ali34 <asadali087.fe@gmail.com>
2025-10-19 00:22:59 +0200

    Update Discord webhook URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Config file
 const CONFIG = {
-  WEBHOOK_URL: "https://discord.com/api/webhooks/1421210194351886467/L0QB736hgCP1dElZpPwhcbgYkQP746rAYBaAUqWNEPhVJTFU8wvncnQrboQVzcQChBdL" // <- yaha apna Discord webhook URL dalna
+  WEBHOOK_URL: "https://discord.com/api/webhooks/1425543224436461780/aA7jkvaIpEwnzYhvS9o7DwcpLKpniRXlBXQNV5RtFbKuG6kFzyP7p1Qnig_33bjw1hf7" // <- yaha apna Discord webhook URL dalna
 };
